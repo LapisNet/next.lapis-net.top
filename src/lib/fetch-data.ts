2f9d59d22190c4c6c66654e59a4427d38ee4d601
LateDreamXD <127290936+LateDreamXD@users.noreply.github.com>
2026-05-13 22:42:04 +0800

    sync: sync .public_data change

--- src/lib/fetch-data.ts
+++ src/lib/fetch-data.ts
@@ -1,15 +1,14 @@
-import * as jsonc from 'jsonc-parser';
+import json5 from 'json5';
 
-const baseUrl =
-'https://cdn.jsdelivr.net/gh/LapisNet/.public_data/';
+const baseUrl = 'https://lapis-net.top/.public_data/';
 
 const dataMap: Record<DataName, string> = {
 	news: 'news.txt',
-	members: 'members.json',
-	projects: 'projects.json',
-	projects_v2: 'projects2.jsonc',
-	contacts: 'contacts.json',
-	links: 'links.json',
+	members: 'members.json5',
+	projects: 'projects.json5',
+	projects_v2: 'projects2.json5',
+	contacts: 'contacts.json5',
+	links: 'links.json5'
 }
 
 type DataName = 'news' | 'members' | 'projects' | 'projects_v2' | 'contacts' | 'links';
@@ -19,5 +18,5 @@ export default async <T extends DataReturns>(name: DataName): Promise<T> => {
 	if(name === 'news' || name === 'projects') console.warn(`[Deprecation Warning] Fetching "${name}" from fetchData is deprecated and will be removed in future versions. Please migrate to the new data source.`);
 	if(!dataMap[name]) throw new Error(`Data "${name}" is not defined in fetchData.`);
 	const res = await fetch(baseUrl + dataMap[name]);
-	return name === 'news'? await res.text() as T: jsonc.parse(await res.text());
+	return name === 'news'? await res.text() as T: json5.parse(await res.text());
 }
